0c5bdce4d4f05d415326d402fef2af5daffbee89
LatellaL <83371530+LatellaL@users.noreply.github.com>
2021-09-08 16:05:16 -0400

    commit tree 868270dfcba581aab23a82f46714ab8ddf16083c

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,27 @@
 def unsafe?(speed)
 
+   # returns true if speed is greater than 60 
+  if speed > 60 
+    true
+
+   # returns true if speed is less than 40 
+  elsif  speed < 40 
+    true
+   # returns false if the speed is between 40 and 60 
+  else 
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+
+   # returns true if speed is greater than 60
+   # returns true if speed is less than 40
+   # returns false if the speed is between 40 and 60
+   # uses the ternary operator 
+  speed < 40 || speed > 60 ? true : false
 end
 	
 
